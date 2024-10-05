8ceb6492750105a98c5a15a4bc5eab0c33428818
chacutpon <94590860+chacutpon@users.noreply.github.com>
2024-10-05 21:16:14 +0700

    Add files via upload

--- models/Users.js
+++ models/Users.js
@@ -14,21 +14,4 @@ const userSchema = mongoose.Schema({
    
 },{timestamps: true}) //จะโชว์ created,updated
 
-userSchema.statics.findByUid = async function(uid) {
-    try {
-        const firestore = admin.firestore(); // ใช้ admin.firestore() จาก firebase.js (ฝั่งหลังบ้าน)
-        const userProfileRef = firestore.collection('players').doc(uid); 
-        const userProfileSnapshot = await userProfileRef.get();
-
-        if (!userProfileSnapshot.exists) {
-            return null; // หรือ handle กรณีไม่พบข้อมูลตามความเหมาะสม
-        }
-
-        const userProfileData = userProfileSnapshot.data();
-        return userProfileData;
-    } catch (err) {
-        console.error('Error fetching user profile from Firestore:', err);
-        throw err; // ส่ง error ต่อเพื่อให้ controller หรือส่วนอื่นๆ จัดการได้
-    }
-};
 module.exports = mongoose.model('Users',userSchema) //(ชื่อโมเดล,schema)
